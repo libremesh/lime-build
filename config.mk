fab74c3e2af29dccec7163bebf0e6ae09af48374
Gui Iribarren <gui@altermundi.net>
2013-06-11 22:24:25 +0200

    separate repo urls and other config variables in a config.mk

---
+++ config.mk
@@ -0,0 +1,12 @@
+OWRT_SCM = git clone git://nbd.name/openwrt.git
+OWRT_PKG_SCM =  git clone git://nbd.name/packages.git
+LIME_GIT_RW = git@github.com:libremesh/lime-packages.git
+LIME_GIT_RO = git://github.com/libremesh/lime-packages.git
+LIME_GIT_BRANCH ?= master
+BUILD_DIR = build
+CONFIG_DIR = configs
+MY_CONFIGS = $(BUILD_DIR)/configs
+IMAGES = images
+SHELL = bash
+COMMUNITY ?= LibreMesh
+SCRIPTS_DIR= scripts
