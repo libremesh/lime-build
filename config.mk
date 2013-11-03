00121626ff42a7a7d04bdccc38d43a205a5d91f3
Gui Iribarren <gui@altermundi.net>
2013-11-03 03:09:27 +0100

    add missing hyphen of libre-mesh in config.mk urls

--- config.mk
+++ config.mk
@@ -1,12 +1,12 @@
 OWRT_SCM = git clone https://github.com/libre-mesh/openwrt.git
 OWRT_PKG_SCM =  git clone https://github.com/libre-mesh/openwrt-packages.git
-LIME_GIT_RW = git@github.com:libremesh/lime-packages.git
-LIME_GIT_RO = git://github.com/libremesh/lime-packages.git
+LIME_GIT_RW = git@github.com:libre-mesh/lime-packages.git
+LIME_GIT_RO = git://github.com/libre-mesh/lime-packages.git
 LIME_GIT_BRANCH ?= master
 BUILD_DIR = build
 CONFIG_DIR = configs
 MY_CONFIGS = $(BUILD_DIR)/configs
 IMAGES = images
 SHELL = bash
-COMMUNITY ?= LibreMesh
+COMMUNITY ?= Libre-Mesh
 SCRIPTS_DIR= scripts
