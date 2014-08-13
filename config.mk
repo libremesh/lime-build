92ff34d001acb29df376cca5a92844f2ba60eea3
Gui Iribarren <gui@altermundi.net>
2014-08-13 05:12:35 -0300

    checkout branch '14.08' both on openwrt base and lime-packages repos

--- config.mk
+++ config.mk
@@ -1,8 +1,8 @@
 DEV ?=
-OWRT_SCM = git clone https://github.com/libre-mesh/openwrt.git
+OWRT_SCM = git clone https://github.com/libre-mesh/openwrt.git -b 14.08
 LIME_GIT_RW = git@github.com:libre-mesh/lime-packages.git
 LIME_GIT_RO = git://github.com/libre-mesh/lime-packages.git
-LIME_GIT_BRANCH ?= 14.07-rc2
+LIME_GIT_BRANCH ?= 14.08
 BUILD_DIR = build
 CONFIG_DIR = configs
 MY_CONFIGS = $(BUILD_DIR)/configs
