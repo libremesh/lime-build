cbe4869db638417cfe21d85a87573958a33fd07f
Gui Iribarren <gui@altermundi.net>
2015-05-05 19:54:59 -0300

    prepare new release: pull 15.04 branch from our repos

--- config.mk
+++ config.mk
@@ -1,8 +1,8 @@
 DEV ?=
-OWRT_SCM = git clone https://github.com/libre-mesh/openwrt.git -b 14.08
+OWRT_SCM = git clone https://github.com/libre-mesh/openwrt.git -b 15.04
 LIME_GIT_RW = git@github.com:libre-mesh/lime-packages.git
 LIME_GIT_RO = git://github.com/libre-mesh/lime-packages.git
-LIME_GIT_BRANCH ?= 14.08
+LIME_GIT_BRANCH ?= 15.04
 BUILD_DIR = build
 CONFIG_DIR = configs
 MY_CONFIGS = $(BUILD_DIR)/configs
