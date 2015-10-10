bc8feb75407c6c93ffb629377744c1a51eb08f3b
Gui Iribarren <gui@altermundi.net>
2015-10-09 23:48:02 -0300

    Release 15.09: pull 15.09 branch from our repos

--- config.mk
+++ config.mk
@@ -1,8 +1,8 @@
 DEV ?=
-OWRT_SCM = git clone https://github.com/libre-mesh/openwrt.git -b 15.04
+OWRT_SCM = git clone https://github.com/libre-mesh/openwrt.git -b 15.09
 LIME_GIT_RW = git@github.com:libre-mesh/lime-packages.git
 LIME_GIT_RO = git://github.com/libre-mesh/lime-packages.git
-LIME_GIT_BRANCH ?= 15.04
+LIME_GIT_BRANCH ?= 15.09
 BUILD_DIR = build
 CONFIG_DIR = configs
 MY_CONFIGS = $(BUILD_DIR)/configs
