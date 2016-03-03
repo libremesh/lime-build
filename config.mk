b47acd9555d9a03d237eb9de7c206a80b63090bb
Gui Iribarren <gui@altermundi.net>
2016-03-03 20:57:59 +0100

    pull everything from upstream on develop branch

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 DEV ?=
-OWRT_SCM = git clone https://github.com/libre-mesh/openwrt.git
+OWRT_SCM = git clone git://git.openwrt.org/15.05/openwrt.git
 LIME_GIT_RW = git@github.com:libre-mesh/lime-packages.git
 LIME_GIT_RO = git://github.com/libre-mesh/lime-packages.git
 LIME_GIT_BRANCH ?= develop
