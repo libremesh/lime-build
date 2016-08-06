670ed07b5610335870e7dff12898db04c3d659c9
Gui Iribarren <gui@altermundi.net>
2016-08-06 19:00:00 +0200

    Release 16.07: pull 16.07 branch from our repos

--- config.mk
+++ config.mk
@@ -1,10 +1,10 @@
 # config.mk
 DEV ?=
-OWRT_GIT ?= https://github.com/libre-mesh/openwrt.git -b 15.09
+OWRT_GIT ?= https://github.com/libre-mesh/openwrt.git -b 16.07
 OWRT_SCM = git clone $(OWRT_GIT)
 LIME_GIT_RW = git@github.com:libre-mesh/lime-packages.git
 LIME_GIT_RO = git://github.com/libre-mesh/lime-packages.git
-LIME_GIT_BRANCH ?= 15.09
+LIME_GIT_BRANCH ?= 16.07
 BUILD_DIR = build
 CONFIG_DIR = targets
 MY_CONFIGS = $(BUILD_DIR)/configs
